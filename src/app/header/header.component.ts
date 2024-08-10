29a008b1dfdcb896790f15a0a253f8d916666a32
RohanJRao <rohanrao360@gmail.com>
2024-08-10 15:59:14 +0530

    Font awesome installed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,20 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
- isOurWorkVisible:boolean=false;
- isServicesVisible:boolean=false;
- isCountriesVisible:boolean=false;
- isAboutVisible:boolean=false;
- toggleOurWork(isvisible:boolean){
-  this.isOurWorkVisible=isvisible;
- }
- toggleServices(isvisible:boolean){
-  this.isServicesVisible=isvisible;
- }
- toggleCountries(isvisible:boolean){
-  this.isCountriesVisible=isvisible;
- }
- toggleAbout(isvisible:boolean){
-  this.isAboutVisible=isvisible;
- }
+  isOurWorkVisible: boolean = false;
+  isServicesVisible: boolean = false;
+  isCountriesVisible: boolean = false;
+  isAboutVisible: boolean = false;
+  
+  toggleOurWork(isvisible: boolean) {
+    this.isOurWorkVisible = isvisible;
+  }
+  toggleServices(isvisible: boolean) {
+    this.isServicesVisible = isvisible;
+  }
+  toggleCountries(isvisible: boolean) {
+    this.isCountriesVisible = isvisible;
+  }
+  toggleAbout(isvisible: boolean) {
+    this.isAboutVisible = isvisible;
+  }
 }
