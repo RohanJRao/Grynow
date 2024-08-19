c254f92c42e58d61ccce6530a621aa13638f208f
esh8050 <meswarreddy1996@gmail.com>
2024-08-19 22:49:53 +0530

    Adding marketing-company component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MarketingCompanyComponent } from './marketing-company/marketing-company.component';
+import { MarketingExpertsComponent } from './marketing-experts/marketing-experts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,MarketingCompanyComponent],
+  imports: [RouterOutlet,HeaderComponent,MarketingCompanyComponent,MarketingExpertsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
